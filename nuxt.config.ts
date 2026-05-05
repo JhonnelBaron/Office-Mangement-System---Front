9e3984468f41f1109b9f85c3fdf574dbd544d624
JhonnelBaron <jhonnelbaron03@gmail.com>
2026-05-05 09:54:19 +0800

    setup: nuxt configuration for prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false, // <--- Siguraduhin na false ito para sa Hostinger
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   runtimeConfig: {
     public: {
-      apiBase: ''
+      // @ts-ignore
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || '' 
     }
   },
   plugins: [
